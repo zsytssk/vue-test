51fe3e448ee963c9192df284171303eecfd14d84
zsytssk <zsytssk@gmail.com>
2025-04-18 09:46:13 +0800

    [dev]

--- src/components/zTable/index.vue
+++ src/components/zTable/index.vue
@@ -14,9 +14,9 @@
         :page-size="pageInfo.pageSize"
         :page-sizes="[10, 30, 50, 100]"
         :total="pageInfo.total"
+        v-bind="paginationProps"
         @current-change="handleCurrentChange"
-        @size-change="handleSizeChange"
-        v-bind="paginationProps" />
+        @size-change="handleSizeChange" />
     </div>
   </div>
 </template>
@@ -28,24 +28,26 @@ import { ref } from 'vue'
 defineOptions({
   name: 'ZTable',
 })
-defineExpose({ run: getTableData, reload: getTableData, reset })
 
 const props = defineProps<{
   boxClass?: string
   tableProps?: Partial<TableProps<any>>
   paginationProps?: Partial<PaginationProps>
-  requestFn: (PageInfo: PageInfo) => Promise<{
-    data: any[]
-    page: number
-    pageSize: number
-    total?: number
-  }>
+  requestFn: (PageInfo: PageInfo) => Promise<
+    | {
+        data: any[]
+        page: number
+        pageSize: number
+        total: number
+      }
+    | undefined
+  >
 }>()
 
 export type PageInfo = {
   page: number
   pageSize: number
-  total?: number
+  total: number
 }
 
 const pageInfo = ref<PageInfo>({ page: 1, pageSize: 10, total: 0 })
@@ -53,6 +55,12 @@ const tableData = ref<any[]>([])
 // 查询
 async function getTableData() {
   const res = await props.requestFn(pageInfo.value)
+
+  if (!res) {
+    tableData.value = []
+    pageInfo.value.total = 0
+    return
+  }
   tableData.value = res.data
   pageInfo.value = {
     total: res.total,
@@ -68,6 +76,7 @@ async function reset() {
     page: 1,
     pageSize: 10,
   }
+  getTableData()
 }
 
 // 分页
@@ -81,6 +90,13 @@ const handleCurrentChange = (val: number) => {
   pageInfo.value.page = val
   getTableData()
 }
+
+defineExpose({
+  run: getTableData,
+  reload: getTableData,
+  reset,
+  pageInfo: pageInfo,
+})
 </script>
 
 <style lang="scss" module>
---
+++ src/page/test1.vue
@@ -0,0 +1,44 @@
+<template>
+  <el-table :data="tableData">
+    <el-table-column
+      align="center"
+      label="创建时间"
+      prop="CreatedAt">
+      <template #default="scope">
+        <span>{{ formatDate(scope.row.CreatedAt) }}</span>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script setup lang="ts">
+import type { TableColumnCtx } from 'element-plus'
+import { ref } from 'vue'
+
+// 定义表格行数据的类型
+interface TableRow {
+  CreatedAt: string
+  // 可按需添加其他属性
+}
+
+// 定义表格列的类型
+type TableColumn = TableColumnCtx<TableRow>
+
+// 定义插槽作用域对象的类型
+interface TableSlotScope {
+  row: TableRow
+  column: TableColumn
+  $index: number
+}
+
+// 模拟表格数据
+const tableData = ref<TableRow[]>([
+  { CreatedAt: '2024-01-01' },
+  { CreatedAt: '2024-01-02' },
+])
+
+// 格式化日期的函数
+const formatDate = (date: string) => {
+  return date
+}
+</script>
